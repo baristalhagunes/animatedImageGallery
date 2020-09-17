7b580f10bef6760ca497b3f8dfb76e6a7079e3a2
baristalhagunes <60845583+baristalhagunes@users.noreply.github.com>
2020-09-17 12:32:07 +0300

    animated galeery added

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "gallery" >
+        <
+        div className = "image-box" >
+        <
+        img src = { require('./7285.jpg') }
+        /> <
+        /div> <
+        div className = "image-box" >
+        <
+        img src = { require('./7285.jpg') }
+        /> <
+        /div> <
+        div className = "image-box" >
+        <
+        img src = { require('./7285.jpg') }
+        /> <
+        /div> <
+        div className = "image-box" >
+        <
+        img src = { require('./7285.jpg') }
+        /> <
+        /div> <
+        div className = "image-box" >
+        <
+        img src = { require('./7285.jpg') }
+        /> <
+        /div> <
+        /div>
+    );
+}
+
+export default App;
